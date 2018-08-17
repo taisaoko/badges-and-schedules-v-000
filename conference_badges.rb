5cbe8dbaad9926075aa976924bd3e3998fe00bca
taisaoko <ttd3calvin@gmail.com>
2018-08-17 14:46:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map{|name|"Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+  room = 0
+  attendees.map do |name| 
+    room += 1
+   "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(attendees)
+  
+end
